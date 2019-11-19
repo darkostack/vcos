d8a24cd751d3b0e5fb25cc9aaa9da1eb4247d5d5
darkopancev <darkopancev.dev@gmail.com>
2019-11-19 16:10:01 +0800

    fixed indentation

--- include/vcos/types.h
+++ include/vcos/types.h
@@ -81,14 +81,14 @@ typedef struct vcTimer {
 } vcTimer;
 
 #ifndef KERNEL_MAXTHREADS
-#define KERNEL_MAXTHREADS   (32)
+#define KERNEL_MAXTHREADS (32)
 #endif
 
-#define KERNEL_PID_UNDEF    (0)
-#define KERNEL_PID_FIRST    (KERNEL_PID_UNDEF + 1)
-#define KERNEL_PID_LAST     (KERNEL_PID_FIRST + KERNEL_MAXTHREADS - 1)
+#define KERNEL_PID_UNDEF (0)
+#define KERNEL_PID_FIRST (KERNEL_PID_UNDEF + 1)
+#define KERNEL_PID_LAST  (KERNEL_PID_FIRST + KERNEL_MAXTHREADS - 1)
 
-#define PRIkernel_pid       PRIi16
+#define PRIkernel_pid PRIi16
 
 /**
  * Optional flags for controlling a threads initial state.
