bc04ff2c9796ccc5be152d448a28a3da67e58498
darkopancev <darkopancev.dev@gmail.com>
2019-11-21 15:24:32 +0800

    change target link libraries to PRIVATE

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -6,6 +6,7 @@ set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 set(CMAKE_ASM_COMPILER arm-none-eabi-as)
 set(CMAKE_RANLIB arm-none-eabi-ranlib)
 
+if (${PLATFORM_NAME} STREQUAL sirius)
 set(CMAKE_EXE_LINKER_FLAGS 
     "-mthumb -mcpu=cortex-m3 -mfloat-abi=soft -Wl,--gc-sections -specs=nano.specs -specs=nosys.specs" 
     CACHE INTERNAL "")
@@ -19,3 +20,4 @@ set(CMAKE_CXX_FLAGS
     "-mcpu=cortex-m3 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -fno-strict-aliasing -fno-builtin -fshort-enums"
     CACHE INTERNAL ""
 )
+endif()
